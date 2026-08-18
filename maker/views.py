1555a4e4ec9ce88190d29342306e082260d393ca
kimybin <kybsophie0504@gmail.com>
2026-08-18 12:33:40 +0900

    ♻️ AUTH_USER_MODEL을 accounts.User로 통합

--- maker/views.py
+++ maker/views.py
@@ -80,7 +80,7 @@ class RankingView(APIView):
                 "count": row["count"],
                 "title": sample.title if sample else None,
                 "description": sample.description if sample else None,
-                "creator": sample.user.username if sample and sample.user else None,
+                "creator": sample.user.nickname if sample and sample.user else None,
             })
         return Response(result)
 
